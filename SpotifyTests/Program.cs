aaa5414ed3e3f4def1463a9932231752f9ca8483
Senne Van Rompaey <vanrompaey.senne1@gmail.com>
2019-04-07 14:55:14 +0200

    Mess around with it myself

--- SpotifyTests/Program.cs
+++ SpotifyTests/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Models;
 
 namespace SpotifyTests
 {
@@ -6,7 +10,53 @@ namespace SpotifyTests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UsefulStuff.GetPlaylistByUrl("https://open.spotify.com/user/svr333-be/playlist/3B70EOUreoxQiFNi1v7VIE?si=ypo4zGUaQqOF6HgKxxYccA");
+
+            /*var privateAccount = GetPrivateAccount("BQDxlwlekTTglEMdIJeXQrqKznMaKEwhpP0dQOd1RBLfKnTbWeugdSpieYgkfx5RnoWaitb6isM0cm821Crb-lCAQfHI04CMFOwR1N6DSd_3fNXl8jAjTUdLquVJUL7shgqbYOhz26X6u2mXScVhSA").Result;
+            if (privateAccount is null) { Console.WriteLine("Account not found."); }
+
+            var publicAccount = GetPublicAccount("svr333-be");
+
+            Console.WriteLine(privateAccount.Id);*/
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Gets the account name by token.
+        /// </summary>
+        public static async Task<PrivateProfile> GetPrivateAccount(string token)
+        {
+            SpotifyWebAPI api = new SpotifyWebAPI
+            {
+                UseAuth = true,
+                AccessToken = token,
+                TokenType = "Bearer"
+            };
+
+            PrivateProfile profile = await api.GetPrivateProfileAsync();
+            if (!profile.HasError()) { return profile; }
+
+            return null;
+        }
+
+        public static async Task<PublicProfile> GetPublicAccount(string name)
+        {
+            SpotifyWebAPI api = new SpotifyWebAPI { UseAuth = false };
+            var profile = api.GetPublicProfile(name);
+            Console.WriteLine(profile.Id);
+            return profile;
+        }
+
+        public static async Task GetPlaylist(string userId, string playlistId)
+        {
+            SpotifyWebAPI api = new SpotifyWebAPI
+            {
+                UseAuth = true,
+                AccessToken = "BQDxlwlekTTglEMdIJeXQrqKznMaKEwhpP0dQOd1RBLfKnTbWeugdSpieYgkfx5RnoWaitb6isM0cm821Crb-lCAQfHI04CMFOwR1N6DSd_3fNXl8jAjTUdLquVJUL7shgqbYOhz26X6u2mXScVhSA",
+                TokenType = "Bearer"
+            };
+            var playlist = api.GetPlaylist(userId, playlistId);
+            Console.WriteLine($"{playlist.Tracks.Total}");
         }
     }
 }
---
+++ SpotifyTests/UsefulStuff.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SpotifyTests
+{
+    public static class UsefulStuff
+    {
+        private static string _userId;
+        private static string _playlistId;
+
+        public static void GetPlaylistByUrl(string url)
+        {
+            if (url.StartsWith("https://open.spotify.com"))
+            {
+                var parts = url.Split('/', StringSplitOptions.RemoveEmptyEntries);
+                _userId = parts[3];
+                _playlistId = parts[5].Split("?si=")[0];
+            }
+
+            else if (url.StartsWith("spotify:user:"))
+            {
+                var parts = url.Split(':');
+                _userId = parts[2];
+                _playlistId = parts[4];
+            }
+
+            else return;
+
+            Console.WriteLine($"{_userId} | {_playlistId}");
+            Program.GetPlaylist(_userId, _playlistId);
+        }
+    }
+}
